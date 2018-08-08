71c86e2b43ae665c41854112f2ad82031176b7f8
Richard Fenix <richard.fenix@gmail.com>
2018-08-08 19:07:20 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+  @@ships = []
+
+  attr_accessor :name, :type, :booty
+
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships.clear
+  end
+
 end
