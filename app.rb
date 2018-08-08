47bdfb263e015ee9c475757bba7788edd334f1eb
Richard Fenix <richard.fenix@gmail.com>
2018-08-08 19:09:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,10 +15,13 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
+      binding.pry
       params[:pirate][:ships].each do |details|
         Ship.new(details)
+        binding.pry
       end
       @ships = Ship.all
+      binding.pry
       erb :'pirates/show'
     end
 
